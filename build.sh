de6c596d61ef3ffe54735ff2a66a72336dc6a637
kanghuayang <kanghuayang@hoolix.com>
2016-10-20 13:24:33 +0800

    更新编译脚本

--- build.sh
+++ build.sh
@@ -3,9 +3,9 @@
 RELEASE_TAG=${RELEASE_TAG:-latest}
 
 echo "building master ..."
-docker build -t es-cluster-master:$RELEASE_TAG -f es.dockerfile --build-arg ES_CONFIG_FILE=config/es-master.yml .
+docker build -t es-cluster-master:$RELEASE_TAG -f Dockerfile --build-arg ES_CONFIG_FILE=config/es-master.yml .
 
 echo "building slave ..."
-docker build -t es-cluster-data:$RELEASE_TAG   -f es.dockerfile --build-arg ES_CONFIG_FILE=config/es-data.yml .
+docker build -t es-cluster-data:$RELEASE_TAG   -f Dockerfile --build-arg ES_CONFIG_FILE=config/es-data.yml .
 
 echo "done"
